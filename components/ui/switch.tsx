5c319ce258938baa661d2b8cfdd3a557c423e3be
Yashik <yashik.2105.ya@gmail.com>
2026-02-11 15:35:57 +0530

    dependency fix

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
 import { cn } from "@/lib/utils"
 
 // Radix is not installed, so I need to implement a simple Switch without Radix or install it.
